c7241c4a13463708f9bb843480ba5ce8cae54957
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-09-27 19:55:32 -0500

    Form to create objects

--- Objects/forms.py
+++ Objects/forms.py
@@ -2,4 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 
 class Create_Objects(forms.Form):
-    user = forms.ChoiceField(User)
+    name = forms.CharField(label='Nombre',max_length=50,widget=forms.TextInput({'class' : 'object-name'}))
+    stock = forms.IntegerField(label='Cantidad',max_value=10000,widget=forms.Select({'class' : 'object-stock'}))
+    description = forms.CharField(label='Descripcion',max_length=500,widget=forms.Textarea({'class' : 'object-description'}))
+    image = forms.ImageField(label='Imagen',widget=forms.Select('class' : 'object-image'))
