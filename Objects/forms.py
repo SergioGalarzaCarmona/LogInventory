35e5784526ec40645426bf62eb22673ced89e45f
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-09-27 19:56:41 -0500

    Update form to create objects

--- Objects/forms.py
+++ Objects/forms.py
@@ -5,4 +5,4 @@ class Create_Objects(forms.Form):
     name = forms.CharField(label='Nombre',max_length=50,widget=forms.TextInput({'class' : 'object-name'}))
     stock = forms.IntegerField(label='Cantidad',max_value=10000,widget=forms.Select({'class' : 'object-stock'}))
     description = forms.CharField(label='Descripcion',max_length=500,widget=forms.Textarea({'class' : 'object-description'}))
-    image = forms.ImageField(label='Imagen',widget=forms.Select('class' : 'object-image'))
+    image = forms.ImageField(label='Imagen',widget=forms.Select({'class' : 'object-image'}))
