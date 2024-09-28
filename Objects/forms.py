6be851c7500c45c47d4fc3191ea8f6bcdfb8a0d0
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-09-28 17:01:28 -0500

    Update main page

--- Objects/forms.py
+++ Objects/forms.py
@@ -4,9 +4,9 @@ from Objects.models import Objects
 
 class Create_or_Modifie_Object(forms.Form):
     name = forms.CharField(label='Nombre',max_length=50,widget=forms.TextInput({'class' : 'object-name'}))
-    stock = forms.IntegerField(label='Cantidad',max_value=10000,widget=forms.Select({'class' : 'object-stock'}))
+    stock = forms.IntegerField(label='Cantidad',max_value=10000,widget=forms.NumberInput({'class' : 'object-stock'}))
     description = forms.CharField(label='Descripcion',max_length=500,widget=forms.Textarea({'class' : 'object-description'}))
-    image = forms.ImageField(label='Imagen',widget=forms.Select({'class' : 'object-image'}))
+    image = forms.ImageField(label='Imagen')
     
 class Untrack_Object(forms.Form):
     pass
--- Objects/views.py
+++ Objects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from Objects import forms,models
+from Objects.forms import Create_or_Modifie_Object,Reverse_Transaction,Untrack_Object
+from Objects.models import Objects,Transactions,Type_Transaction
 # Create your views here.
 def main(request):
-    return render(request,'main.html')
+    return render(request,'main.html',{
+        'create_or_modifie_form' : Create_or_Modifie_Object,
+        'reverse_transaction_form' : Reverse_Transaction,
+        'untrack_object_form' : Untrack_Object
+    })
