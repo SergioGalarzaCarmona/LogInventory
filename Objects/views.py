234219e296d84bbbb9081de348edd3e7d25d2f06
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-09-30 21:30:15 -0500

    Fixing error of reload

--- Objects/views.py
+++ Objects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from Objects.forms import ObjectForm
 from Objects.models import Objects,Transactions,Type_Transaction
 # Create your views here.
@@ -15,11 +15,7 @@ def main(request):
             post = form.save(commit=False)
             post.user_id = request.user
             post.save()
-            object_instance = Objects.objects.filter(user_id = request.user)
-            return render(request,'main.html',{
-                'create_or_modifie_form' : ObjectForm,
-                'objects' : object_instance,
-            })
+            return redirect('main')
         else:
             object_instance = Objects.objects.filter(user_id = request.user)
             return render(request,'main.html',{
