a1be8313be8511cf94505313cd23b4e581481512
Santiago Medina López <smedina@iegabo.edu.co>
2024-10-07 17:09:12 -0500

    createForm styles

---
+++ Users/static/scripts/mainScript.js
@@ -0,0 +1,14 @@
+const name = document.getElementById("id_name");
+name.setAttribute("class", "name");
+
+const stock = document.getElementById("id_stock");
+stock.setAttribute("class", "stock");
+
+const description = document.getElementById("id_description");
+description.setAttribute("class", "description");
+
+const image = document.getElementById("id_image");
+image.setAttribute("class", "image");
+
+
+
