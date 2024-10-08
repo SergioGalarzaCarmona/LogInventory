cb6d194ea5f4099421f7b9902235fb5ee94e0ab9
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-07 20:50:12 -0500

    Fixing error with the files in main

---
+++ Objects/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Objects(models.Model):
+    object_id = models.IntegerField(primary_key=True,editable=False,unique=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    show_object = models.BooleanField(default=True)
+    stock = models.IntegerField(null=False,blank=False)
+    last_change = models.DateTimeField(max_length=50,auto_now_add=True)
+    description = models.TextField(max_length=500)
+    image = models.ImageField(blank=True,upload_to='object_images/',default='object_images/image.png')
+
+class Type_Transaction(models.Model):
+    type_id = models.IntegerField(primary_key=True,editable=False,unique=True)
+    status = models.CharField(max_length=20,null=False,blank=False)
+
+class Transactions(models.Model):
+    transaction_id = models.IntegerField(primary_key=True,editable=False,unique=True)
+    object_id = models.ForeignKey(Objects,on_delete=models.PROTECT,editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    type_transaction = models.ForeignKey(Type_Transaction,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True,editable=False)
+    stock_before = models.IntegerField(null=False,blank=False)
+    stock_after = models.IntegerField(null=False,blank=False)
