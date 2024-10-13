5d9abcd44d384878534327775309678787e2d996
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-12 19:40:59 -0500

    Create restart password

--- Users/views.py
+++ Users/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login,logout,authenticate
+from random import randint
 
+#To send email
+import os
+from dotenv import load_dotenv
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+
+
+
+#passkey to reset password
+#This passkey is to validate email sent
+caracters = [
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3',
+'4', '5', '6', '7', '8', '9'
+]
+passkey = ''
+for x in range(25):
+    indice = randint(0,35)
+    caracter = caracters[indice]
+    passkey += caracter
+        
 # Create your views here.
 def sign_up(request):
     if request.method == 'GET':
@@ -17,11 +42,11 @@ def sign_up(request):
                 return redirect('main')
             except IntegrityError:
                 return render(request,'sign_up.html',{
-                'error' : "User already exists"
+                'error' : "El nombre de usuario ya existe."
                 })
         else:
             return render(request,'sign_up.html',{
-                'error' : "Password don't match"
+                'error' : "Las contraseñas no coinciden."
                 })
             
             
@@ -29,10 +54,10 @@ def log_in(request):
     if request.method == 'GET':
         return render(request,'log_in.html')
     else:
-        user = authenticate(username=request.POST['user'],password=request.POST['password'])
+        user = authenticate(email=request.POST['user'],password=request.POST['password'])
         if user is None:
             return render(request,'log_in.html',{
-            'error' : "User doesn't exists"
+            'error' : "El usuario o la contraseña son incorrectos."
         })
         else:
             login(request,user)
@@ -44,3 +69,42 @@ def log_out(request):
     
 def home(request):
     return render(request,'home.html')
+
+
+def mail_sender(request):
+    if request.method == 'GET':
+        return render(request,'last_password.html')
+    else:
+        #Load the variables of enviroment
+        load_dotenv()
+        app_password = os.getenv('APP_PASSWORD')
+        
+        #Email adress with that send email
+        email_sender = 'Log.Inventory2406@gmail.com'
+        
+        #subject and body of email
+        subject = 'Email para cambiar la contraseña.'
+        body = f'''En este email se adjunta un link para restablecer tu contraseña.
+        NO COMPARTAS ESTE LINK CON NADIE.
+        Si has recibido este mensaje y no lo solicitaste, ps preocupese pq no tenemos forma de cambiar la contraseña de otra manera. 
+
+        http://127.0.0.1:8000/change%20password/{passkey}
+
+
+        '''
+        #config of EmailMessage
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = request.POST['email']
+        em['Subject'] = subject
+        em.set_content = body
+
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+            smtp.login(email_sender,app_password)
+            smtp.sendmail(email_sender,request.POST['email'],em.as_string())
+
+
+
+        return render(request,'last_password.html')
