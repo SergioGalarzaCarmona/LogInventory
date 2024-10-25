cf637d44d98dc1e5e1b7f5583872abc37b440c9a
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-25 15:07:00 -0500

    Fixing button to delete image

--- Objects/views.py
+++ Objects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from Objects.forms import ObjectForm,ModifieObject
 from Objects.models import Objects,Transactions,Type_Transaction
+from django.contrib.auth.models import AnonymousUser
 
 #This view show space work, here show all objects
 def main(request):
@@ -14,11 +15,11 @@ def main(request):
             'show' : False
         })
     else:
-        
+
         #Craate a instance to the model "Objects" with the form
         form = ObjectForm(request.POST,request.FILES)
         #Check that the name of objects doesn't exist
-        instance_valid = Objects.objects.filter(name = request.POST['name'], user_id = request.user)
+        instance_valid = Objects.objects.filter(name = request.POST['name'], user_id = request.user,show_object = True)
         #Verifie that the stock of object isn't negative
         if int(request.POST['stock']) < 0:
             return render(request,'main.html',{
@@ -35,9 +36,9 @@ def main(request):
                 post = form.save(commit=False)
                 post.user_id = request.user
                 post.save()
-                
                 #Create a instance into the record
-                object_instance = Objects.objects.get(name = request.POST['name'])
+                object_instance_list = Objects.objects.filter(name = request.POST['name'])
+                object_instance = object_instance_list[len(object_instance_list) - 1]
                 type_instance = Type_Transaction.objects.get(type_id = 3) 
                 Transactions.objects.create(object_id = object_instance ,user_id = request.user, type_transaction = type_instance,stock_before = 0, stock_after = request.POST['stock'] )
                 return redirect('main')
@@ -63,24 +64,27 @@ def main(request):
 def object_instance(request,id):
     #Filter record with the object id. 
     record_object = Transactions.objects.filter(object_id = id)
+    #filter obejct with this id
+    object_instance = Objects.objects.get(object_id = id)
     if request.method == 'GET':
-        #filter obejct with this id
-        object_instance = Objects.objects.get(object_id = id)
-        form = ModifieObject(instance=object_instance)
         return render(request,'objects.html',{
             'object' : object_instance,
             'message' : 'Los cambios hechos en los campos de "nombre","descripción" y "imagen", no podrán ser retrocedidos.¡CUIDADO!',
             'record_object' : record_object[::-1]
         })
     else:
+        #Validate if the input "image_clear" have some value, the varibale don't raise a exception
+        try:
+            validation_image_button = str(request.POST['image_clear'])
+        except:
+            validation_image_button = None
         # If the user click on "Retroceder cambios" enter on "if"
         try:
             validation_record_button = request.POST['object']
         except:
             validation_record_button = None
+        
         if validation_record_button != None:
-            #Filter the object by the object id 
-            object_instance = Objects.objects.get(object_id = request.POST['object'])
             #Filter which transaction will be used by the trasaction id
             transaction_instance = Transactions.objects.get(transaction_id = request.POST['transaction'])
             #Go back the stock with the information of transaction
@@ -95,9 +99,17 @@ def object_instance(request,id):
             #save object with all changes
             object_instance.save()
             return redirect('main')
+        #if the clear image button is clicked, change image to default iamge
+        elif validation_image_button == 'on':
+            object_instance.image = 'object_images/image.png'
+            #save change 
+            object_instance.save()
+            return render(request,'objects.html',{
+                'object' : object_instance,
+                'message' : 'Los cambios hechos en los campos de "nombre","descripción" y "imagen", no podrán ser retrocedidos.¡CUIDADO!',
+                'record_object' : record_object[::-1]
+            })
         else:
-            #Filter object by id to show its info into the form
-            object_instance = Objects.objects.get(object_id = id)
             
             #Create form to modifie the before instance
             form = ModifieObject(request.POST,request.FILES,instance=object_instance)
@@ -107,7 +119,8 @@ def object_instance(request,id):
                 
                 return render(request,'objects.html',{
                 'object' : object_instance,
-                'message' : 'El stock no puede ser negativo, ingresa un numero mayor o igual a 0.',
+                'message' : 'Los cambios hechos en los campos de "nombre","descripción" y "imagen", no podrán ser retrocedidos.¡CUIDADO!',
+                'change_invalid' : 'El stock no puede ser negativo.',
                 'record_object' : record_object[::-1]
             })
                 
@@ -116,12 +129,16 @@ def object_instance(request,id):
                 
             except:
                 validation_name_exists = []
-            if len(validation_name_exists) != 0:
-                    return render(request,'objects.html',{
-                        'object' : object_instance,
-                        'message' : 'El nombre que ingreso ya existe, ingrese otro por favor.',
-                        'record_object' : record_object[::-1]
-            })
+            if len(validation_name_exists) == 1:
+                    object_filter = validation_name_exists[0]
+                    if object_instance.object_id != object_filter.object_id:
+                        return render(request,'objects.html',{
+                            'object' : object_instance,
+                            'message' : 'Los cambios hechos en los campos de "nombre","descripción" y "imagen", no podrán ser retrocedidos.¡CUIDADO!',
+                            'change_invalid' : 'El nombre que ingreso ya esta en uso, por favor ingrese otro.',
+                            'record_object' : record_object[::-1]})
+                    else: 
+                        pass
             #Validation that data 'image' exists
             try:
                 validation_image_file = request.FILES['image']
@@ -188,20 +205,29 @@ def object_instance(request,id):
                 #Create transacion with form data
                 Transactions.objects.create(object_id = object_instance,user_id = request.user,type_transaction = type_instance,stock_before = stock_before,stock_after = request.POST['stock'])
                 return redirect('main') 
+
 def record(request):
     if request.method =='GET':
+        #filter all transaction that that user have maked
         transactions = Transactions.objects.filter(user_id = request.user)
         return render(request,'record.html',{
+            # reverse a list to show the last change at top
             'record' : transactions[::-1],
         })
     else: 
+        #filter object instance and transactions instance by them id
         object_instance = Objects.objects.get(object_id = request.POST['object'])
         transaction_instance = Transactions.objects.get(transaction_id = request.POST['transaction'])
+        #Save stock before to change current stock 
         object_instance.stock = transaction_instance.stock_before
+        #if show_object is 0, change to 1
         if object_instance.show_object == 0:
             object_instance.show_object = 1
+        #Type instance is to declare a reversed change
         type_instance = Type_Transaction.objects.get(type_id = 5)
+        #Create instance in record
         Transactions.objects.create(object_id = object_instance,user_id = request.user,type_transaction = type_instance, stock_before = transaction_instance.stock_after, stock_after = transaction_instance.stock_before  )
+        #Save all change into DB
         object_instance.save()
         return redirect('main')
         
--- Users/static/scripts/objectScript.js
+++ Users/static/scripts/objectScript.js
@@ -1,6 +1,4 @@
 function triggerSubmit() {
-    document.getElementById('update').click();
-    const delete_object = document.getElementById('id_show_object')
-    delete_object.checked = true;
-} 
+        document.getElementById('update').click();
+    }
 
