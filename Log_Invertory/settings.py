5c29ae7dd8137f9bce2c1210ac85e4e95787fa9f
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-27 11:22:08 -0500

    Fix settings.py

--- Log_Invertory/settings.py
+++ Log_Invertory/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'Log_Invertory.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite///db.sqlite3',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600    
     )
 #     {
