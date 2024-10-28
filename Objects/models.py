bf125ada2333b92ee5aca166ee0e3a8ab43e1df5
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-27 22:36:11 -0500

    Upgrade database

--- Objects/models.py
+++ Objects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.utils.timezone import now
 
 #Creating object "Objects"
 class Objects(models.Model):
@@ -26,6 +27,6 @@ class Transactions(models.Model):
     object_id = models.ForeignKey(Objects,on_delete=models.PROTECT,editable=False)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     type_transaction = models.ForeignKey(Type_Transaction,on_delete=models.CASCADE)
-    date = models.DateTimeField(default=datetime.today())
+    date = models.DateTimeField(default=now())
     stock_before = models.IntegerField(null=False,blank=False)
     stock_after = models.IntegerField(null=False,blank=False)
