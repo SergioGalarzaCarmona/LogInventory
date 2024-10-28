52446ab5357266b1452bdedab07f5764a45fb899
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-27 21:31:49 -0500

    Update view.py from objects

--- Objects/views.py
+++ Objects/views.py
@@ -6,19 +6,17 @@ from Objects.models import Objects,Transactions,Type_Transaction
 number_objects = Objects.objects.all()
 number_trasactions = Transactions.objects.all()
 
-type_transactions = Type_Transaction.objects.all()
-if len(type_transactions) == 0:
-    Type_Transaction.objects.create(type_id = 1,status = 'Add')
-    Type_Transaction.objects.create(type_id = 2,status = 'Substract')
-    Type_Transaction.objects.create(type_id = 3,status = 'Create')
-    Type_Transaction.objects.create(type_id = 4,status = 'Properties')
-    Type_Transaction.objects.create(type_id = 5,status = 'Go back')
-    Type_Transaction.objects.create(type_id = 6,status = 'Delete')
 
 #This view shows space work, and shows all objects
 def main(request):
     
     if request.method == 'GET':
+        Type_Transaction.objects.create(type_id = 1,status = 'Add')
+        Type_Transaction.objects.create(type_id = 2,status = 'Substract')
+        Type_Transaction.objects.create(type_id = 3,status = 'Create')
+        Type_Transaction.objects.create(type_id = 4,status = 'Properties')
+        Type_Transaction.objects.create(type_id = 5,status = 'Go back')
+        Type_Transaction.objects.create(type_id = 6,status = 'Delete')
         print(Type_Transaction.objects.all())
         if str(request.user) != 'AnonymousUser':
             #Filter objects by user
