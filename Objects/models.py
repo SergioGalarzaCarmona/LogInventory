1391134aff1d4b7f115fad521ac94dfdac9bd4c2
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2024-10-28 19:58:10 -0500

    Change objects

--- Objects/models.py
+++ Objects/models.py
@@ -10,7 +10,6 @@ class Objects(models.Model):
     name = models.CharField(max_length=50)
     show_object = models.BooleanField(default=True)
     stock = models.IntegerField(null=False,blank=False)
-    last_change = models.DateTimeField(auto_now_add=True)
     description = models.TextField(max_length=500)
     image = models.ImageField(blank=True,upload_to='object_images/',default='object_images/image.png')
     
